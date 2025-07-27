ad91706afaa8774678f9da6173d128b180adc3a4
Aakash <82360703+Aakash-Meharda@users.noreply.github.com>
2025-07-27 11:48:58 +0530

    Update save-user.js

--- netlify/functions/save-user.js
+++ netlify/functions/save-user.js
@@ -1,5 +1,5 @@
 const { neon } = require('@neondatabase/serverless');
-const sql = neon(process.env.DATABASE_URL);
+const sql = neon("postgresql://neondb_owner:npg_3YbZqPAEaNG2@ep-billowing-sea-a7nrltdl-pooler.ap-southeast-2.aws.neon.tech/neondb?sslmode=require&channel_binding=require");
 
 exports.handler = async (event) => {
   if (event.httpMethod !== 'POST') {
